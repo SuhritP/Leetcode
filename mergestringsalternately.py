8fa505d803d55d134071a0531f81331bb31e10c3
Suhrit Padakanti <suhritp@gmail.com>
2025-06-26 11:56:19 -0700

    Merge Strings Alternately

---
+++ mergestringsalternately.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        mergeStr = ""
+        if (len(word1) > len(word2)):
+            for i in range(len(word2)):
+                mergeStr+= word1[i] + word2[i]
+            mergeStr += word1[i + 1: len(word1)]
+        else:
+            for j in range(len(word1)):
+                mergeStr+= word1[j] + word2[j]
+            mergeStr += word2[j + 1: len(word2)]
+        return mergeStr
+
+
+
+# identify the longer string and shorter string and based on which is longer, loop through the shorter till the end to merge and then add the 
+# remaining string back to the merged 
